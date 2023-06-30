b882d0736f0ce13d319bc6a30c31eecc72aee751
JoaoMRL <ribeiroleite.joaomarco@gmail.com>
2023-06-30 14:49:08 +0200

    Utilisation et test d'Input

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -7,9 +7,13 @@ import { Room } from '../entities';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent {
-  room:Room[] =[
+  list:Room[] =[
     {id:1,name:"living room",surface:123,opened:true},
     {id:2,name:"bed room",surface:80,opened:true},
     {id:3,name:"Bathroom",surface:60,opened:false},
   ];
+  nRoom:Room ={id:0,name:'',surface:0,opened:false};
+  newRoom(){
+    this.list.push(this.nRoom);
+  }
 }
